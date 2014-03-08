4ef60a5e9b6aab3600c8242195f896bb65db4570
Stephen Riesenberg <sjohnr@users.noreply.github.com>
2014-03-08 11:39:53 -0600

    Correct path to bootstrap.php

--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@ define('ENVIRONMENT', 'dev'); // or 'prod'
 define('IS_DEBUG',    true);
 
 // bootstrap the framework
-include(ROOT_DIR.'/lib/bootstrap.php');
+include(ROOT_DIR.'/bootstrap.php');
 
 // dispatch the request
 Controller::getInstance()->dispatch();
